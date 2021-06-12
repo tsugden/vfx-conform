1883f7b228e76c8229338ffecf1115afe18ad5cf
Tom Sugden <tom@tomsugden.uk>
2021-06-12 15:06:59 +0100

    backend api added

---
+++ backend/api/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/ping", (req, res) => {
+  res.json({
+    ping: "pong",
+  });
+});
+
+app.listen(5000, () => {
+  console.log("App listening on port 5000");
+});
